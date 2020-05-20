f4fef53ec513bb6b6b9214626642f12e06c10998
vienpham2019 <vienpham2019@gmail.com>
2020-05-19 23:23:13 -0500

    save

--- maze_generation.js
+++ maze_generation.js
@@ -1,5 +1,8 @@
-let width = 1800 
-let height = 800 
+let size = 40 
+// let width = size * 70
+// let height = size * 40
+let width = size * 35
+let height = size * 20
 let canvas = document.getElementById("maze")
 canvas.width = width
 canvas.height = height
@@ -8,7 +11,6 @@ canvas.style.background = "black"
 let c = canvas.getContext("2d")
 let nodes = []
 
-let size = 40 
 let cols = width / size 
 let rows = height / size 
 
@@ -18,8 +20,10 @@ let visited_nodes = []
 let stack = []
 
 let quere = []
-let visited_nodes_for_path = []
+let finish_path = false 
+let nodes_path = []
 let path = []
+let current_node 
 let start_node, end_node 
 
 let myReq
@@ -81,17 +85,18 @@ const Node = function(x , y , neighbor_node = [], prev_node = null){
     }
 }
 
-const Block = function(x , y , prev_node = null){
+const Block = function(x , y , color = "red", prev_node = null){
     this.x = x 
     this.y = y 
     this.dx = size
     this.dy = size
     this.prev_node  = prev_node
+    this.color = color
 
-    this.draw = (color = "red") => {
+    this.draw = () => {
         c.beginPath()
         c.rect(this.x + size / 8 , this.y + size / 8, size * (3 / 4), size * (3 /4))
-        c.fillStyle = color
+        c.fillStyle = this.color
         c.fill()
     }
 
@@ -177,9 +182,9 @@ const setUp = () => {
     }
 
     block = new Block(0 , 0)
-    start_node = new Block(0 , 0)
+    start_node = new Block(0 , 0 , "blue")
     quere.push(start_node)
-    end_node = new Block(width - size,height - size)
+    end_node = new Block(width - size,height - size , "green")
     draw_maze()
 }
 
@@ -187,22 +192,26 @@ const setUp = () => {
 const draw_maze = () => {
     // setTimeout(() => {
         myReq = requestAnimationFrame(draw_maze)
-    // }, 500);
+    // }, 100);
     c.clearRect(0,0,canvas.width, canvas.height)
     for(let i = 0; i < nodes.length; i ++){
         nodes[i].draw()
     }
 
+    for(let i = 0; i < nodes_path.length; i ++){
+        nodes_path[i].draw()
+    }
+
     if(stack.length === 0){
-        start_node.draw("blue")
-        end_node.draw("green")
+        start_node.draw()
+        end_node.draw()
         if(end_node.prev_node){
+            find_path()
+        }else if(finish_path){
+            console.log("end")
             cancelAnimationFrame(myReq)
         }else{
             solve_maze()
-            for(let i = 0; i < visited_nodes_for_path.length; i ++){
-                visited_nodes_for_path[i].draw("yellow")
-            }
         }
     }else{ 
         block.move()  
@@ -211,10 +220,8 @@ const draw_maze = () => {
 
 const solve_maze = () => {
     let quere_nodes = [...quere]
-    let current_node = quere.shift()
-    visited_nodes_for_path.push(current_node)
-    console.log(quere)
-    console.log(visited_nodes_for_path)
+    quere.shift()
+    // visited_nodes_for_path.push(current_node)
     if(end_node.prev_node){
         return
     }
@@ -225,67 +232,83 @@ const solve_maze = () => {
 
 const check_neighbor_node = (node) => {
     let x = node.x
-    let y = node.y 
+    let y = node.y
+    let color = "purple"
     // top 
     let top = nodes.find(n => n.x === x + (size / 2) && n.y === y - (size / 2))
-    if(top && !top.walls[2] && !visited_nodes_for_path.find(n => n.x === top.x  - (size / 2)&& n.y === top.y + (size / 2))){
-        console.log("top")
+    if(top && !top.walls[2] && !nodes_path.find(n => n.x === top.x  - (size / 2) && n.y === top.y - (size / 2))){
         let top_x = top.x  - (size / 2)
-        let top_y = top.y  + (size / 2)
+        let top_y = top.y  - (size / 2)
         if(top_x === end_node.x && top_y === end_node.y){
             end_node.prev_node = node 
+            current_node = node 
             return 
         }else{
-            let top_block = new Block(top_x, top_y, node)
+            let top_block = new Block(top_x, top_y, color, node)
             quere.push(top_block)
+            nodes_path.push(top_block)
         }
     }
 
     // right
     let right = nodes.find(n => n.x === x + (size * 3 / 2) && n.y === y + (size / 2))
-    if(right && !right.walls[3] && !visited_nodes_for_path.find(n => n.x === right.x - (size / 2) && n.y === right.y - (size / 2))){
-        console.log("right")
+    if(right && !right.walls[3] && !nodes_path.find(n => n.x === right.x - (size / 2) && n.y === right.y - (size / 2))){
         let right_x = right.x - (size / 2)
         let right_y = right.y - (size / 2)
         if(right_x === end_node.x && right_y === end_node.y){
             end_node.prev_node = node 
+            current_node = node 
             return 
         }else{
-            let right_block = new Block(right_x, right_y, node)
+            let right_block = new Block(right_x, right_y, color ,node)
             quere.push(right_block)
+            nodes_path.push(right_block)
         }
     }
 
     // bottom
     let bottom = nodes.find(n => n.x === x + (size / 2) && n.y === y + (size * 3 / 2) ) 
-    if(bottom && !bottom.walls[0] && !visited_nodes_for_path.find(n => n.x === bottom.x - (size / 2) && n.y === bottom.y  - (size / 2))){
-        console.log("bottom")
+    if(bottom && !bottom.walls[0] && !nodes_path.find(n => n.x === bottom.x - (size / 2) && n.y === bottom.y  - (size / 2))){
         let bottom_x = bottom.x - (size / 2)
         let bottom_y = bottom.y - (size / 2)
         if(bottom_x === end_node.x && bottom_y === end_node.y){
             end_node.prev_node = node 
+            current_node = node 
             return 
         }else{
-            let bottom_block = new Block(bottom_x, bottom_y, node)
+            let bottom_block = new Block(bottom_x, bottom_y, color ,node)
             quere.push(bottom_block)
+            nodes_path.push(bottom_block)
         }
     }
 
     // left 
     let left = nodes.find(n => n.x === x - (size / 2) && n.y === y + (size / 2))
-    if(left && !left.walls[1] && !visited_nodes_for_path.find(n => n.x === left.x  - (size / 2) && n.y === left.y - (size / 2))){
-        console.log("left")
+    if(left && !left.walls[1] && !nodes_path.find(n => n.x === left.x  - (size / 2) && n.y === left.y - (size / 2))){
         let left_x = left.x - (size / 2)
         let left_y = left.y - (size / 2)
         if(left_x === end_node.x && left_y === end_node.y){
             end_node.prev_node = node 
+            current_node = node 
             return 
         }else{
-            let left_block = new Block(left_x,left_y, node)
+            let left_block = new Block(left_x,left_y, color , node)
             quere.push(left_block)
+            nodes_path.push(left_block)
         }
     }
 
 }
 
+const find_path = () => {
+    current_node.color = "green"
+    if(current_node.x === 0 && current_node.y === 0){
+        finish_path = true
+        return
+    }
+    current_node = current_node.prev_node
+}
+
 setUp()
+
+console.log(nodes)
