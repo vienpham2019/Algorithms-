4d6f05aebd3c29d420c83945f5d94bc87d9c6ca5
vienpham2019 <vienpham2019@gmail.com>
2020-06-14 00:46:50 -0500

    save

--- maze_generation_divide.js
+++ maze_generation_divide.js
@@ -1,9 +1,10 @@
-let size = 20 
+let size = 30 
 // let width = size * 70
 // let height = size * 40
-let width = size * 45
-let height = size * 40
+let width = size * 60
+let height = size * 30
 let canvas = document.getElementById("maze")
+let button = document.getElementById('solve_maze_btn')
 canvas.width = width
 canvas.height = height
 canvas.style.background = "black"
@@ -86,6 +87,88 @@ const Node = function(x , y , neighbor_node = [], prev_node = null){
     }
 }
 
+const Block = function(x , y , color = "red", prev_node = null){
+    this.x = x 
+    this.y = y 
+    this.dx = size
+    this.dy = size
+    this.prev_node  = prev_node
+    this.color = color
+
+    this.draw = () => {
+        c.beginPath()
+        c.rect(this.x - (size / 2), this.y - (size / 2), size - 3, size - 3)
+        c.fillStyle = this.color
+        c.fill()
+    }
+
+    this.move = () => {
+        let current_node = stack[0]
+        let neighbor_nodes = []
+        let x = current_node.x
+        let y = current_node.y
+
+        // top
+        if(y - size > 0 && !visited_nodes.find(n => n.x === x && n.y === y - size)){
+            let top = nodes.find(n => n.x === x && n.y === y - size)
+            neighbor_nodes.push(top)
+        }
+
+        // right 
+        if(x + size < width && !visited_nodes.find(n => n.x === x + size && n.y === y)){
+            let right = nodes.find(n => n.x === x + size && n.y === y)
+            neighbor_nodes.push(right)
+        }
+
+        // bottom
+        if(y + size < height && !visited_nodes.find(n => n.x === x && n.y === y + size)){
+            let bottom = nodes.find(n => n.x === x && n.y === y + size)
+            neighbor_nodes.push(bottom)
+        }
+
+        // left
+        if(x - size > 0 && !visited_nodes.find(n => n.x === x - size && n.y === y)){
+            let left = nodes.find(n => n.x === x - size && n.y === y)
+            neighbor_nodes.push(left)
+        }
+
+        if(neighbor_nodes.length > 0){
+            let next_node = neighbor_nodes[Math.floor(Math.random() * neighbor_nodes.length)]
+            stack.unshift(next_node)
+            visited_nodes.push(next_node)
+            this.dx = next_node.x - current_node.x
+            this.dy = next_node.y - current_node.y
+
+            this.x += this.dx 
+            this.y += this.dy
+
+            if(this.dx > 0) { // right 
+                current_node.walls[1] = false 
+                next_node.walls[3] = false 
+            }else if(this.dx < 0){ // left
+                current_node.walls[3] = false 
+                next_node.walls[1] = false 
+            } 
+
+            if(this.dy > 0) { // down
+                current_node.walls[2] = false 
+                next_node.walls[0] = false 
+            }else if(this.dy < 0){ // up
+                current_node.walls[0] = false 
+                next_node.walls[2] = false 
+            }
+        }else{
+            let dx = current_node.x  - (this.x + (size / 2))
+            let dy = current_node.y  - (this.y + (size / 2))
+            this.x += dx 
+            this.y += dy
+            stack.shift()
+        }
+        this.draw()
+    }
+
+}
+
 const setUp = () => {
     for(let i = 0; i < rows ; i ++){
         for(let j = 0; j < cols ; j ++){
@@ -103,19 +186,28 @@ const setUp = () => {
             }else if(j === cols - 1 && i != rows - 1){
                 node.walls[1] = true
             }
+
+            if(i === 0 && j === 0){
+                stack.push(node)
+                visited_nodes.push(node)
+            }
             nodes.push(node)
         }
     }
 
-    // start_node = new Block(0 , 0 , "blue")
-    // quere.push(start_node)
-    // end_node = new Block(width - size,height - size , "green")
+    start_node = new Block(size / 2,size / 2, "blue")
+    quere.push(start_node)
+    end_node = new Block((cols - 1) * size + (size / 2),( rows - 1 ) * size + (size / 2) , "green")
     draw_maze(nodes , cols, rows, 0, 0)
+    
+    button.addEventListener('click' , () => {
+       run_solve_maze()
+    })
 }
 
 const draw_maze = (nodes_array , x_max, y_max, x_min , y_min) => {
 
-    if(x_max - x_min < 1 || y_max - y_min < 1) return 
+    if(x_max - x_min < 1 || y_max - y_min < 1) return
 
     let random_x = getRandom(x_min,x_max)
     let random_y = getRandom(y_min,y_max)
@@ -129,9 +221,8 @@ const draw_maze = (nodes_array , x_max, y_max, x_min , y_min) => {
                 && nodes_array[i].y >= y_min * size + (size / 2)
                 && nodes_array[i].x != (cols - 1) * size + (size / 2)
                 ){
-                nodes_array[i].walls[1] = true
-                if (nodes_array[i].y === random_y * size + (size / 2)){
-                    nodes_array[i].walls[1] = false
+                if (nodes_array[i].y != random_y * size + (size / 2)){
+                    nodes_array[i].walls[1] = true
                 }
             }
         }else{
@@ -140,9 +231,8 @@ const draw_maze = (nodes_array , x_max, y_max, x_min , y_min) => {
                 && nodes_array[i].x <= x_max * size + (size / 2)
                 && nodes_array[i].x >= x_min * size + (size / 2)
                 ){
-                nodes_array[i].walls[2] = true
-                if(nodes_array[i].x === random_x * size + (size / 2)){
-                    nodes_array[i].walls[2] = false
+                if(nodes_array[i].x != random_x * size + (size / 2)){
+                    nodes_array[i].walls[2] = true
                 }
             }
         }
@@ -158,6 +248,123 @@ const draw_maze = (nodes_array , x_max, y_max, x_min , y_min) => {
             draw_maze(nodes_array, x_max, y_max, x_min , random_y + 1) // bottom
         }
     }, 100);
+}
+
+const run_solve_maze = () => {
+    myReq = requestAnimationFrame(run_solve_maze)
+    c.clearRect(0,0,canvas.width, canvas.height)
+
+    for(let i = 0; i < nodes.length; i ++){
+        nodes[i].draw()
+    }
+
+    for(let i = 0; i < nodes_path.length; i ++){
+        nodes_path[i].draw()
+    }
+
+    start_node.draw()
+    end_node.draw()
+    if(end_node.prev_node){
+        find_path()
+    }
+    if(finish_path){
+        cancelAnimationFrame(myReq)
+    }
+    if(!end_node.prev_node && !finish_path){
+        solve_maze()
+    }
+}
+
+const solve_maze = () => {
+    let quere_nodes = [...quere]
+    quere.shift()
+    // visited_nodes_for_path.push(current_node)
+    if(end_node.prev_node){
+        return
+    }
+    for(let i = 0; i < quere_nodes.length ; i ++){
+        check_neighbor_node(quere_nodes[i])
+    }
+}
+
+const check_neighbor_node = (node) => {
+    let x = node.x
+    let y = node.y
+    let color = "MidnightBlue"
+    let current__find_node = nodes.find(c_n => c_n.x === node.x && c_n.y === node.y )
+    // top 
+    let top = nodes.find(n => n.x === x && n.y === y - size)
+    if(top && !current__find_node.walls[0] && !nodes_path.find(n => n.x === top.x && n.y === top.y)){
+        let top_x = top.x
+        let top_y = top.y
+        if(top_x === end_node.x && top_y === end_node.y){
+            end_node.prev_node = node 
+            current_node = node 
+            return 
+        }else{
+            let top_block = new Block(top_x, top_y, color, node)
+            quere.push(top_block)
+            nodes_path.push(top_block)
+        }
+    }
+
+    // right
+    let right = nodes.find(n => n.x === x + size && n.y === y )
+    if(right && !current__find_node.walls[1] && !nodes_path.find(n => n.x === right.x  && n.y === right.y)){
+        let right_x = right.x 
+        let right_y = right.y 
+        if(right_x === end_node.x && right_y === end_node.y){
+            end_node.prev_node = node 
+            current_node = node 
+            return 
+        }else{
+            let right_block = new Block(right_x, right_y, color ,node)
+            quere.push(right_block)
+            nodes_path.push(right_block)
+        }
+    }
+
+    // bottom
+    let bottom = nodes.find(n => n.x === x && n.y === y + size ) 
+    if(bottom && !current__find_node.walls[2] && !nodes_path.find(n => n.x === bottom.x  && n.y === bottom.y )){
+        let bottom_x = bottom.x 
+        let bottom_y = bottom.y 
+        if(bottom_x === end_node.x && bottom_y === end_node.y){
+            end_node.prev_node = node 
+            current_node = node 
+            return 
+        }else{
+            let bottom_block = new Block(bottom_x, bottom_y, color ,node)
+            quere.push(bottom_block)
+            nodes_path.push(bottom_block)
+        }
+    }
+
+    // left 
+    let left = nodes.find(n => n.x === x - size && n.y === y)
+    if(left && !current__find_node.walls[3] && !nodes_path.find(n => n.x === left.x && n.y === left.y)){
+        let left_x = left.x 
+        let left_y = left.y 
+        if(left_x === end_node.x && left_y === end_node.y){
+            end_node.prev_node = node 
+            current_node = node 
+            return 
+        }else{
+            let left_block = new Block(left_x,left_y, color , node)
+            quere.push(left_block)
+            nodes_path.push(left_block)
+        }
+    }
+
+}
+
+const find_path = () => {
+    current_node.color = "green"
+    if(current_node.x === start_node.x && current_node.y === start_node.y){
+        finish_path = true
+        return
+    }
+    current_node = current_node.prev_node
 }
 
 const getRandom = (min,max) => {
